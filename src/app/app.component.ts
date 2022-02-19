3da8a88bf56062de3e2d6631513cc8ec46d1dcbb
NiAlvarez <nicolas.alvarez@fluxit.com.ar>
2022-02-18 21:09:09 -0300

    se agregan 3 pipes comunes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  nombre : string = 'nicolas alvarez'
+
+  valor: number = 1000
+
+  obj = {
+    nombre: 'Nicolas'
+  }
+
+  cambiarNombre(){
+    console.log(this.nombre)
+    console.log(this.valor)
+  }
+
   title = 'pipesApp';
 }
